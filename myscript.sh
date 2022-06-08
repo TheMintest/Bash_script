de1740904257ac26f1c32ea8f7d4dc26ca220c1b
Mintest <g.dharcourt@posteo.net>
2022-06-08 16:12:40 +0200

    Boucle dans un dossier, récupère la Hauteur et la largeur de l'image

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
 
-FILES=/Users/muttedit/Desktop/PHOTOS\ TUTO/INSTAGRAM
+FOLDER=/Users/muttedit/Desktop/PHOTOS_TUTO/INSTAGRAM
 
-
-echo "File: $FILES"
-HEIGHT=`sips -g pixelHeight $FILE`
-echo "Height: $HEIGHT"
+x=1
+for entry in "$FOLDER"/*
+do
+    echo "FILE number $x is $(basename "$entry")"
+    HEIGHT=`sips -g pixelHeight $entry`
+    WIDTH=`sips -g pixelWidth $entry`
+    echo $WIDTH $HEIGHT
+     x=$((x+1))
+done
+# echo "File: $FILES"
+# HEIGHT=`sips -g pixelHeight $FILE`
+# WIDHT=`sips -g pixelWidth $FILE`git remote add origin git@github.com:TheMintest/Bash_script.git
+# echo "Height: $HEIGHT"
