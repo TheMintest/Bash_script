e67d00244c12f8c773dd539214151d9925b9115f
Mintest <g.dharcourt@posteo.net>
2022-06-09 12:05:46 +0200

    Je comprend rien pourquoi mon elif marche pas !

--- Cultura_Checker.sh
+++ Cultura_Checker.sh
@@ -7,32 +7,29 @@ FOLDER=/Users/muttedit/Desktop/PHOTOS_TUTO
 
 x=1
 for entry in "$FOLDER"/*
-
 do
-    # echo "Processing $entry"
-    #check if the file is an image file
-    if [[ $entry == *.jpg ]] || [[ $entry == *.png ]] || [[ $entry == *.jpeg ]]|| [[ $entry == *.tiff ]]
-    then
-        echo "File number $x is $(basename "$entry") and is valid"
-        HEIGHTFULL=`sips -g pixelHeight $entry`
-        HEIGHT=`echo $HEIGHTFULL | cut -d ':' -f2`
-        checkParentFolder()
-         if [[ $PARENTFOLDER == *"/INSTAGRAM"* ]]
-    then
-        checkInstagramSize()
-      elif [[ $PARENTFOLDER == *"/HD"* ]]
-      then
-        checkHDsize()
-      elif [[ $PARENTFOLDER == *"/CULTURA.COM"* ]]
-        then
-          checkCultura.comSize()
-        fi
-      fi
+  #check if the file is an image file
+  if [ $entry == *.jpg ] || [ $entry == *.png ] || [ $entry == *.jpeg ]|| [ $entry == *.tiff ] ; then
+    echo "File number $x is $(basename "$entry") and is valid"
+    #get the dimensions of the image and put them in a variable
+    HEIGHTFULL=`sips -g pixelHeight $entry`
+    HEIGHT=`echo $HEIGHTFULL | cut -d ':' -f2`
+    WIDTHFULL=`sips -g pixelWidth $entry`
+    WIDTH=`echo $WIDTHFULL | cut -d ':' -f2`
+    checkParentFolder()
+    if [$PARENTFOLDER == *"/INSTAGRAM"*] ; then 
+        echo "File number $x is in the INSTAGRAM folder"
+        checkInstagramSize() ;
+    elif [ $PARENTFOLDER == *"/HD"*] ; then
+        checkHDsize() ;
+    elif [ $PARENTFOLDER == *"/CULTURA.COM"* ] ; then
+        checkCultura.comSize() ;
+    elif
+        echo "File number $x is $(basename "$entry") and is not in a valid folder"
     fi
+  fi
+  
 
-
-       
-      fi
      x=$((x+1))
 done
 
@@ -44,10 +41,19 @@ checkParentFolder() {
 }
 
 checkInstagramSize() {
-    if [[ $HEIGHT -gt 1080 ]]
+    if [[ $HEIGHT -eq 1080 ]] && [[ $WIDTH -eq 1080 ]]
     then
-        echo "File is too big"
+        echo "Instagram size is correct for $(basename "$entry")"
     else
-        echo "File is not too big"
+        echo "Instagram size is not correct for file $(basename "$entry")"
+    fi
+}
+
+checkHDsize() {
+    if [[ $HEIGHT -eq 1920 ]] && [[ $WIDTH -eq 1080 ]]
+    then
+        echo "HD size is correct for $(basename "$entry")"
+    else
+        echo "HD size is not correct for file $(basename "$entry")"
     fi
 }
